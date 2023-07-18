65a8aaba5d3afb6beead4cc92c8d4277b19d970e
Christopher Bloch von Blottnitz <christophervonblottnitz@gmail.com>
2023-07-18 15:33:58 +0200

    Second commit with working base

---
+++ EisbachApp/app/index.js
@@ -0,0 +1,18 @@
+import {View, ScrollView, SafeAreaView, Text} from 'react-native';
+import {Stack, useRouter} from 'expo-router';
+import {React} from 'react'; //Do I need this?
+
+
+const Home = () => {
+
+
+
+
+    return (
+        <View>
+            <Text>Home</Text>
+        </View>
+    )
+}
+
+export default Home;
